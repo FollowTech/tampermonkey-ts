a35caa872fd31afdac3146c19212fff390d87577
test <test@dell.com>
2023-12-14 02:00:47 -0800

    first commit

---
+++ header/index.ts
@@ -0,0 +1,19 @@
+import { GmFunctions, RunAt, UserScript } from "./UserScript";
+import build from "./build";
+
+const script: UserScript = {
+    name: 'hello world',
+    namespace: 'https://github.com/xcr1234',
+    description: '一个hello world，油猴脚本脚手架',
+    version: '1.0.0',
+    includes: ['*://*'],
+    grants: [
+        GmFunctions.unsafeWindow
+    ],
+    matches: [
+        "https://testvault.dell.com/TestExecution/TestRunDetail*"
+    ],
+    runAt: RunAt.document_end
+}
+
+build(script, 'app_header.js')
